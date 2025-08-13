cf43427d29e55ed700654a3f7bd754247c2b1267
giamm0810-sys <giamm0810@gmail.com>
2025-08-13 17:16:01 +0700

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -19,7 +19,7 @@ self.addEventListener('notificationclick', function(event) {
     
     // Mở trang admin khi click vào notification
     event.waitUntil(
-        clients.openWindow('https://https://lemontea2.netlify.app')
+        clients.openWindow('https://689c6587a6e1a80008cc216a--lemontea2.netlify.app/')
     );
 });
 
